9dbf0bc27f3c0a946083ff5865d91aae015458c3
unknown <tahmidshihab2000@gmail.com>
2022-04-22 19:14:01 +0600

    product information added and updated design of ui

--- js/app.js
+++ js/app.js
@@ -3,25 +3,130 @@ const loadProducts = () => {
   const data = [
     {
       id: 1,
-      title: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
-      price: 109.95,
-      description:
-        "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
-      category: "men's clothing",
-      image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
-      rating: { rate: 3.9, count: 120 },
+      title: "Lucien Piccard",
+      price: 499.95,
+      category: "men's watch",
+      image: "https://www.worldofwatches.com/media/catalog/product/cache/6275b0637049ab4262e9abf2e63a6f54/m/e/mens-leather-brown-transparent-center-dial-watch-lucien-piccard-lp-1300a3_1.jpg",
+      rating: { rate: 4.6, count: 120 },
     },
     {
       id: 2,
-      title: "Mens Casual Premium Slim Fit T-Shirts ",
-      price: 22.3,
-      description:
-        "Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.",
-      category: "men's clothing",
+      title: "MontBlanc",
+      price: 2012.3,
+      category: "men's watch",
       image:
-        "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg",
+        "https://www.worldofwatches.com/media/catalog/product/cache/6275b0637049ab4262e9abf2e63a6f54/m/e/mens-heritage-monopusher-chronograph-leather-silver-dial-watch-montblanc-mb119952_1.jpg",
       rating: { rate: 4.1, count: "259" },
     },
+        {
+      id: 3,
+      title: "Gevril",
+      price: 55.99,
+      category: "men's watch",
+      image: "https://www.worldofwatches.com/media/catalog/product/cache/6275b0637049ab4262e9abf2e63a6f54/m/e/mens-madison-genuine-leather-blue-dial-watch-gevril-y-a24ef_1.jpg",
+      rating: { rate: 4.7, count: 500 },
+    },
+    {
+      id: 4,
+      title: "Gv2 By Gevril",
+      price: 15.99,
+      category: "men's watch",
+      image: "https://www.worldofwatches.com/media/catalog/product/cache/f37af02772093860da70218ca33a6351/w/o/womens-genoa-stainless-steel-mother-of-pearl-dial-watch-gv2-by-gevril-y-a24fr_1.jpg",
+      rating: { rate: 3.1, count: 430 },
+    },
+    {
+      id: 5,
+      title:
+        "Bell And Ross",
+      price: 695,
+      category: "men's watch",
+      image: "https://www.worldofwatches.com/media/catalog/product/cache/6275b0637049ab4262e9abf2e63a6f54/m/e/mens-horolum-chronograph-rubber-grey-dial-watch-bell-and-ross-blrbr0394-gr-st-scar_1.jpg",
+      rating: { rate: 4.6, count: 400 },
+    },
+    {
+      id: 6,
+      title: "Livingston  ",
+      price: 168,
+      category: "men's watch",
+      image: "https://www.worldofwatches.com/media/catalog/product/cache/6275b0637049ab4262e9abf2e63a6f54/m/e/mens-livingston-stainless-steel-ivory-dial-watch-brooklyn-watch-co-bw-101-m1712_1.jpg",
+      rating: { rate: 3.9, count: 70 },
+    },
+    {
+      id: 7,
+      title: "Greenpoint",
+      price: 29.99,
+      category: "men's watch",
+      image: "https://www.worldofwatches.com/media/catalog/product/cache/f37af02772093860da70218ca33a6351/m/e/mens-greenpoint-nylon-black-dial-watch-brooklyn-watch-co-bw-8125q1_1.jpg",
+      rating: { rate: 3.6, count: 400 },
+    },
+    {
+      id: 8,
+      title: "Dunham Rose",
+      price: 100.99,
+      category: "men's watch",
+      image: "https://www.worldofwatches.com/media/catalog/product/cache/6275b0637049ab4262e9abf2e63a6f54/m/e/mens-dunham-stainless-steel-black-skeletal-display-dial-watch-brooklyn-bw-202-m3262_1.jpg",
+      rating: { rate: 3.9, count: 100 },
+    },
+    {
+      id: 9,
+      title: "Bedford Brownstone II",
+      price: 84,
+      category: "men's watch",
+      image: "https://www.worldofwatches.com/media/catalog/product/cache/f37af02772093860da70218ca33a6351/m/e/mens-bedford-brownstone-ii-chronograph-stainless-steel-black-dial-watch-bw-308-blk-3_1.jpg",
+      rating: { rate: 4.9, count: 203 },
+    },
+    {
+      id: 10,
+      title: "Black Dial Watch",
+      price: 72.50,
+      category: "men's watch",
+      image: "https://www.worldofwatches.com/media/catalog/product/cache/f37af02772093860da70218ca33a6351/m/e/mens-bedford-brownstone-ii-chronograph-leather-black-dial-watch-brookly-bw-307-blk-3_1.jpg",
+      rating: { rate: 4.2, count: 470 },
+    },
+    {
+      id: 11,
+      title:
+        "Silicon Power 256GB SSD 3D NAND A55 SLC Cache Performance Boost SATA III 2.5",
+      price: 109,
+      category: "men's watch",
+      image: "https://fakestoreapi.com/img/71kWymZ+c+L._AC_SX679_.jpg",
+      rating: { rate: 4.8, count: 319 },
+    },
+    {
+      id: 12,
+      title:
+        "WD 4TB Gaming Drive Works with Playstation 4 Portable External Hard Drive",
+      price: 114,
+      category: "men's watch",
+      image: "https://fakestoreapi.com/img/61mtL65D4cL._AC_SX679_.jpg",
+      rating: { rate: 4.8, count: 400 },
+    },
+    {
+      id: 13,
+      title: "Acer SB220Q bi 21.5 inches Full HD (1920 x 1080) IPS Ultra-Thin",
+      price: 599,
+      category: "men's watch",
+      image: "https://fakestoreapi.com/img/81QpkIctqPL._AC_SX679_.jpg",
+      rating: { rate: 3.9, count: 250 },
+    },
+    {
+      id: 14,
+      title:
+        "Samsung 49-Inch CHG90 144Hz Curved Gaming Monitor (LC49HG90DMNXZA) – Super Ultrawide Screen QLED ",
+      price: 999.99,
+      category: "men's watch",
+      image: "https://fakestoreapi.com/img/81Zt42ioCgL._AC_SX679_.jpg",
+      rating: { rate: 4.2, count: 140 },
+    },
+    {
+      id: 15,
+      title: "BIYLACLESEN Women's 3-in-1 Snowboard Jacket Winter Coats",
+      price: 56.99,
+      category: "men's watch",
+      image: "https://fakestoreapi.com/img/51Y5NI-I5jL._AC_UX679_.jpg",
+      rating: { rate: 3.6, count: 235 },
+    },
+
 
   ];
   showProducts(data);
@@ -38,18 +143,18 @@ const showProducts = (products) => {
       <div>
     <img class="product-image" src=${image}></img>
       </div>
-      <h3>${product.title}</h3>
+      <h3><b>${product.title}</b></h3>
       
       <p><b>Category:</b> ${product.category}</p>
       
-      <h2>Price: $ ${product.price}</h2>
+      <h3>Price: $ ${product.price}</h3>
 
       <h5><b>Product Rating: ${product.rating.rate}</b></h5>
       <h5><b>Total Reviews: ${product.rating.count}</b></h5>
        
 
-      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
-      <button id="details-btn" class="btn btn-danger"onclick="showDetails(${product.id})">Details</button></div>
+      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-info">Add to Cart</button>
+      </div>
       `;
     document.getElementById("all-products").appendChild(div);
   }
@@ -116,18 +221,5 @@ const updateTotal = () => {
   document.getElementById("total").innerText = grandTotal.toFixed(2);
 };
 
-// show product details
-const showDetails = (id) => {
-  const showDetails = document.getElementById("show-details");
-  fetch(`https://fakestoreapi.com/products/${id}`)
-    .then((res) => res.json())
-    .then((data) => {
-      showDetails.innerHTML = `
-      <h3>Show Details</h3>
-      <p>${data.description}</p>
-      
-      `;
-    });
-};
 
 loadProducts();
